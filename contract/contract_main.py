8a3900fe6187f71e3564f0ab3ee4e78f991c7898
javier-rosas <jr2dj@virginia.edu>
2021-12-23 13:15:04 -0500

    Change to approval and clear names

--- contract/contract_main.py
+++ contract/contract_main.py
@@ -132,10 +132,10 @@ def clear_state_program():
 
 
 if __name__ == "__main__":
-    with open("vote_approval.teal", "w") as f:
+    with open("casa_approval.teal", "w") as f:
         compiled = compileTeal(approval_program(), mode=Mode.Application, version=5)
         f.write(compiled)
 
-    with open("vote_clear_state.teal", "w") as f:
+    with open("casa_clear.teal", "w") as f:
         compiled = compileTeal(clear_state_program(), mode=Mode.Application, version=5)
         f.write(compiled)
